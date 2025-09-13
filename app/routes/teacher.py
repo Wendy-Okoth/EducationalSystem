86855b1507ccf905ee6c9948396b123511875ea6
Wendy-Okoth <wendyaokoth@gmail.com>
2025-09-13 04:39:07 +0300

    Adding features to the teacher dashboard

--- app/routes/teacher.py
+++ app/routes/teacher.py
@@ -9,3 +9,14 @@ def dashboard():
 
     return render_template("teacher_dashboard.html")
    
+@teacher_bp.route('/profile')
+def profile():
+    return render_template('teacher_profile.html')
+
+@teacher_bp.route('/calendar')
+def calendar():
+    return render_template('teacher/calendar.html')
+
+@teacher_bp.route('/subjects')
+def subjects():
+    return render_template('teacher/subjects.html')
